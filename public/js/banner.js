02920b18314f629083f6996860a90f2a821facc0
yongwa1214 <yongwa1214@gmail.com>
2025-05-26 12:20:17 +0900

    [docs] banner 반응형 작업

--- public/js/banner.js
+++ public/js/banner.js
@@ -48,12 +48,11 @@ window.addEventListener("load", function () {
     // 3.  slide 를 생성하고 작동 시킨다.
 
     const swiper = new Swiper(".sw_banner", {
-      slidesPerView: 2,
-      spaceBetween: 25,
+      
       speed: 1500,
       loop: true,
       pagination: {
-        el: ".sw_banner .swiper-pagination",
+        el: ".sw_banner_pg",
         clickable: true,
       },
       navigation: {
@@ -64,7 +63,26 @@ window.addEventListener("load", function () {
         delay: 2500,
         disableOnInteraction: false,
       },
+      // 화면의 해상도에 따라서 작동 
+      breakpoints:{
+        // 760 이상이면
+        760: {
+          slidesPerView:1,
+          spaceBetween: 25,
+        },
+        // 960 이상이면
+        960: {
+          slidesPerView: 2,
+          spaceBetween: 25,
+        },
+      
+        // 1024 이상이면 
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 25,},
+      }
     });
+
 
     const swBanner = this.document.querySelector(".sw_banner");
     swBanner.addEventListener("mouseenter", function () {
